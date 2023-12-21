33ad40b1a907a586e941749187da2b1c0af85a63
MikePzaza_ <pzaza49243002@gmail.com_>
2023-12-21 22:54:02 +0700

    finished

--- Python.py
+++ Python.py
@@ -1,81 +1,79 @@
-
 #This code demonstrate how to show location of hand landmark
 import cv2
 import mediapipe as mp
 
-Nfing = 5
+
 cap = cv2.VideoCapture(0)
 myname = "Flook"  
 #Call hand pipe line module
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
-
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
     #print(results.multi_hand_landmarks)
-
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             for id, lm in enumerate(handLms.landmark):
                 h, w, c = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
+                cx, cy = int(lm.x * w), int(lm.y * h) #lm คือตำแหน่ง
+                if id == 20:
+                    id20 = int(id)
+                    cx20 = cy
+                if id == 18:
+                    id18 = int(id)
+                    cx18 = cy
+                if id == 16:
+                    id16 = int(id)
+                    cx16 = cy
+                if id == 14:
+                    id14 = int(id)
+                    cx14 = cy
+                if id == 12:
+                    id12 = int(id)
+                    cx12 = cy
+                if id == 10:
+                    id10 = int(id)
+                    cx10 = cy
+                if id == 8:
+                    id8 = int(id)
+                    cx8 = cy
+                if id == 6:
+                    id6 = int(id)
+                    cx6 = cy
                 if id == 4:
                     id4 = int(id)
                     cx4 = cx
                 if id == 3:
                     id3 = int(id)
                     cx3 = cx
-                if id == 8:
-                    id8 = int(id)
-                    cy8 = cy
-                if id == 6: 
-                    id6 = int(id)
-                    cy6 = cy
-                if id == 12: 
-                    id12 = int(id)
-                    cy12 = cy
-                if id == 10: 
-                    id10 = int(id)
-                    cy10 = cy  
-                if id == 16: 
-                    id16 = int(id)
-                    cy16 = cy
-                if id == 14: 
-                    id14 = int(id)
-                    cy14 = cy 
-                if id == 20: 
-                    id20 = int(id)
-                    cy20 = cy
-                if id == 18: 
-                    id18 = int(id)
-                    cy18 = cy 
-            if cy8 > cy6:
-                Nfing = 0           
-            elif cy12 > cy10:
-                Nfing = 1        
-            elif cy16 > cy14:
-                Nfing = 2
-            elif cy20 > cy18:
-                Nfing = 3
-            elif cx4 > cx3:
-                Nfing = 4
-            else:
-                Nfing = 5
-
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
-
-    cv2.putText(img, str(int(Nfing)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
-                (255, 0, 255), 3) 
-
-    cv2.putText(img, str(str(myname)), (500, 450), cv2.FONT_HERSHEY_PLAIN, 3,
-                (200, 0, 0), 3)
+            finger = ["THUMB", "Index finger","Middle finger","Ring finger","Pinky Finger"]   
+            if cx20 < cx18 :
+                cv2.putText(img, str(finger[4]),(80, 250), cv2.FONT_HERSHEY_PLAIN, 2,
+                (135, 160, 250), 3)
+               
+            if cx16 < cx14:
+                cv2.putText(img, str(finger[3]),(80, 300 ), cv2.FONT_HERSHEY_PLAIN,2,
+                (135, 160, 250), 3)
+               
+            if cx12 < cx10 :
+                cv2.putText(img, str(finger[2]),(80, 350), cv2.FONT_HERSHEY_PLAIN, 2,
+                (135, 160, 250), 3)
+               
+            if cx8 < cx6 :
+                cv2.putText(img, str(finger[1]),(80, 400), cv2.FONT_HERSHEY_PLAIN, 2,
+                (135, 160, 250), 3)
+                
+            if cx4 > cx3:
+                cv2.putText(img, str(finger[0]),(80, 450), cv2.FONT_HERSHEY_PLAIN, 2,
+                (135, 160, 250), 3)
+              
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)        
+    cv2.putText(img, str(str(myname)), (450, 430), cv2.FONT_HERSHEY_PLAIN, 3,
+                (100, 5, 0), 3)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
-
-
-
 #Closeing all open windows
 #cv2.destroyAllWindows()
